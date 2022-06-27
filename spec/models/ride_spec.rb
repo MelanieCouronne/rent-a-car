8b92e32ece6da8f09b6d25031ef19c769a672aa6
Melanie Couronne <melanie.couronne@gmail.com>
2022-06-27 15:07:35 +0200

    Added associations test to ride spec

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -16,49 +16,55 @@ RSpec.describe Ride, type: :model do
                         rental_rate: 4)
   }
 
-  it "is valid with valid attributes" do
-    # ride = Ride.new(user_id: new_user.id, pickup_location: "Bordeaux", checkout_location: "Lyon", car_model: "Sandero III", car_brand: "Dacia", rental_rate: 4)
-
-    expect(subject).to be_valid
+  context "Associations" do
+    it { should belong_to(:user).without_validating_presence}
+    it { should have_many(:reservations).without_validating_presence}
   end
 
-  it "is not valid without a pickup location" do
-    # ride = Ride.new(pickup_location: nil)
-    # refacto
-    subject.pickup_location = nil
-    expect(subject).to_not be_valid
+  context "Validations" do
+    it "is valid with valid attributes" do
+      # ride = Ride.new(user_id: new_user.id, pickup_location: "Bordeaux", checkout_location: "Lyon", car_model: "Sandero III", car_brand: "Dacia", rental_rate: 4)
+
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a pickup location" do
+      # ride = Ride.new(pickup_location: nil)
+      # refacto
+      subject.pickup_location = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a checkout location" do
+      # ride = Ride.new(checkout_location: nil)
+
+      # refacto
+      subject.checkout_location = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a car model" do
+      # ride = Ride.new(car_model: nil)
+
+      # refacto
+      subject.car_model = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a car brand" do
+      ride = Ride.new(car_brand: nil)
+
+      # refacto
+      subject.car_brand = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a rental rate" do
+      ride = Ride.new(rental_rate: nil)
+
+      # refacto
+      subject.rental_rate = nil
+      expect(subject).to_not be_valid
+    end
   end
-
-  it "is not valid without a checkout location" do
-    # ride = Ride.new(checkout_location: nil)
-
-    # refacto
-    subject.checkout_location = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a car model" do
-    # ride = Ride.new(car_model: nil)
-
-    # refacto
-    subject.car_model = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a car brand" do
-    ride = Ride.new(car_brand: nil)
-
-    # refacto
-    subject.car_brand = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a rental rate" do
-    ride = Ride.new(rental_rate: nil)
-
-    # refacto
-    subject.rental_rate = nil
-    expect(subject).to_not be_valid
-  end
-
 end
